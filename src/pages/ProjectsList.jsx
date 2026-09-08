7b0769be23c7045a9aa744401d57152ecd55a8c2
JeyJey <jey.afandiyev@gmail.com>
2026-09-08 15:59:57 +0200

    Fix: Theme-Schalter lag im DropdownMenuTrigger (React.Children.only)

--- src/pages/ProjectsList.jsx
+++ src/pages/ProjectsList.jsx
@@ -651,6 +651,7 @@ function ProjectsListContent() {
             >
               <Languages className="w-5 h-5" />
             </Button>
+            <ThemeSwitch de={language === 'de'} />
             {/* Profilbild mit Menue: Profil, Abo, Startseite, Abmelden */}
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
@@ -670,7 +671,6 @@ function ProjectsListContent() {
                     {user?.full_name || user?.email?.split('@')[0]}
                   </span>
                 </button>
-            <ThemeSwitch de={language === 'de'} />
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end" className="w-60">
                 <div className="px-2 py-2 border-b border-slate-100 mb-1">
